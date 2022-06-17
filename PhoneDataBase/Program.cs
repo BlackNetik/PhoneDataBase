dcf3303d86c5766e033b6d8c7d37b097297bb708
vladimir ratnikov <ratnikov.ratnikov-vova@yandex.ru>
2022-06-17 23:50:36 +0300

    Внесены небольшие правки

--- PhoneDataBase/Program.cs
+++ PhoneDataBase/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddScoped<IUserService, UserServices>();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -35,6 +36,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
 
